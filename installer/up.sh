a5f170b4e61e07304a2636d64a0a2074beee5ae4
jaka1m <61670480+jaka1m@users.noreply.github.com>
2023-06-06 14:46:58 +0800

    Create up.sh

---
+++ installer/up.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+### Color
+Green="\e[92;1m"
+RED="\033[31m"
+YELLOW="\033[33m"
+BLUE="\033[36m"
+FONT="\033[0m"
+GREENBG="\033[42;37m"
+REDBG="\033[41;37m"
+OK="${Green}--->${FONT}"
+ERROR="${RED}[ERROR]${FONT}"
+GRAY="\e[1;30m"
+NC='\e[0m'
+red='\e[1;31m'
+green='\e[0;32m'
+# ===================
+export GREEN='\033[0;32m'
+export NC='\033[0m'
+clear
+echo ""
+# // Setup
+echo -e "${GREEN}UPDATE HAP!${NC}"
+sleep 3
+sudo apt update
+sudo apt upgrade -y
+sudo apt install haproxy -y
+rm /etc/haproxy/haproxy.cfg
+wget -O /etc/haproxy/haproxy.cfg "https://raw.githubusercontent.com/jaka1m/project/main/xray/haproxy.cfg" >/dev/null 2>&1
+rm -f /root/up.sh > /dev/null 2>&1
+reboot
+echo ""
