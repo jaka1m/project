38a07f4a7acb56227794ab0c8188ee3ea832c158
jaka1m <61670480+jaka1m@users.noreply.github.com>
2023-07-04 18:08:10 +0800

    Update path.sh

--- installer/path.sh
+++ installer/path.sh
@@ -21,6 +21,7 @@ echo ""
 # // Setup
 echo -e "${GREEN}DOWNLOADING FILE SETUP!${NC}"
 sleep 3
-    sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/jaka1m/project/main/premi.sh && chmod +x premi.sh && sed -i -e 's/\r$//' premi.sh && screen -S premi ./premi.sh
+    #sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/jaka1m/project/main/premi.sh && chmod +x premi.sh && sed -i -e 's/\r$//' premi.sh && screen -S premi ./premi.sh
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/jaka1m/project/main/premi.sh && chmod +x premi.sh && screen -S premi ./premi.sh
 rm -f /root/path.sh > /dev/null 2>&1
 echo ""
